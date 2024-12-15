4e9aa2332a9e8c28d6a34e1eed2d8f7b50251554
guillevr777 <gs.villanueva@iesnervion.es>
2024-12-15 16:10:58 +0100

    Exercise done

--- Tema3.2/src/parte1/Ejer6.java
+++ Tema3.2/src/parte1/Ejer6.java
@@ -6,39 +6,40 @@ public class Ejer6 {
 	public static void main(String[]args) {
 		Scanner reader = new Scanner(System.in);
 		
+		//creamos un string para guardar la frase
 		String frase;
 		
+		//pedimos la frase por consola
 		System.out.println("Dime la frase ");
 		frase = reader.nextLine();
 		
+		//si al llamar la funcion devuleve true es palindromma y si es false no lo es
 		if (palindroma(frase)) {
 			System.out.println("Es palindroma!");
 		} else {
 			System.out.println("No lo es...");
 		}
 		
+		//cerramos el escaner
 		reader.close();
 	}
+	//creamos una funcion para averiguar si es palindroma la frase devolviendo un booleano
 	static boolean palindroma(String frase) {
-		int contador = frase.length()-1;
-		boolean respuesta;
-		String fraseDelRevez;
-		
-		for (int i = 0 ; i < frase.length() ; i++) {
-			if (fraseDelRevez[]) {
-				frase += "";
-			} else {
-			frase += frase.charAt(contador);
-			}
-			contador--;
-		}
+		//creamos un string y sustituimos los espacios
+		String fraseSinEspacio = frase.replaceAll(" ", "").toLowerCase();
+		//creamos un booleano que se devolvera al llamar a la funcion
+		boolean respuesta = true;
+		//creamos dos contadores , un ira sumando y otro restando segun las aliteraciones del while
+		int fin = fraseSinEspacio.length()-1;
+		int inicio = 0;
+		//creamos un while para que no salga mientras que inicio sea menor que fin y la respuesta no sea false
+		 while (inicio < fin && respuesta) {
+	            respuesta = (fraseSinEspacio.charAt(inicio) == fraseSinEspacio.charAt(fin));
+	            inicio++;
+	            fin--;
+	        }
+
+		 //devolvemos el booleano
 		return respuesta;
-	}
-	static String invierteCadena(String cadena) {
-		String frase ="";
-		
-		for (int i = cadena.length()-1 ; i < cadena.length() ; i--) {
-			
-		}
 	}
 }
--- Tema3.2/src/parte1/Ejer9.java
+++ Tema3.2/src/parte1/Ejer9.java
@@ -6,23 +6,42 @@ public class Ejer9 {
 	public static void main(String[]args) {
 		Scanner reader = new Scanner(System.in);
 		
-		//creamos la variable que contendra la frase dicha por el usuario
+		//creamos un string para guardar la frase
 		String frase;
 		
-		//creamos un array para albergar el array de la funcion
-		String[] palabras;
-		//preguntamos por la frase
-		System.out.println("Dime una frase y te la ordeno de menor a mayor : ");
+		//pedimos la frase por consola
+		System.out.println("Dime la frase ");
 		frase = reader.nextLine();
 		
-		//llamamos a la funcion
-		palabras = tablaOrdenada(frase);
+		//si al llamar la funcion devuelve true es el idioma javalandia y si es false no lo es
+		if (esJavalandia(frase)) {
+			System.out.println("Es el idioma Javalandia!");
+		} else {
+			System.out.println("No es el idioma Javalandia...");
+		}
 		
-		//creamos un for para recorrer la tabla
-		for (String palabra : palabras) {
-			System.out.print(palabra + " ");
-		} 
 		//cerramos el escaner
 		reader.close();
+	}
+	//creamos una funcion para averiguar si es el idioma javalandia
+	static boolean esJavalandia(String frase) {
+		
+		//creamos un string que quite las pausas del idioma
+		String respaldoFrase = frase.replaceAll(" ", "");
+		
+		//creamos un booleano que se devolvera al llamar a la funcion
+		boolean respuesta = true;
+		
+		//buscamos la muletilla tipica del inicio y del fin de las frases
+		int inicio = respaldoFrase.indexOf("Javalín, javalón");
+		int fin = respaldoFrase.indexOf("javalén, len, len", 1);
+
+		//comprobamos si de verdad es Javalandia
+		if (inicio == -1 && fin == -1) {
+			respuesta = false;
+		}
+		
+		//devolvemos el booleano
+		return respuesta;
 	}
 }
