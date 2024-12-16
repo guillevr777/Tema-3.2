99abc695b091b33722b2fc5cc97bde2906480454
guillevr777 <gs.villanueva@iesnervion.es>
2024-12-16 13:30:21 +0100

    done

---
+++ Tema3.2/src/parte1/Ejer10.java
@@ -0,0 +1,11 @@
+package parte1;
+
+public class Ejer10 {
+
+	public static void main(String[] args) {
+
+		
+		
+	}
+
+}
--- Tema3.2/src/parte1/Ejer9.java
+++ Tema3.2/src/parte1/Ejer9.java
@@ -26,19 +26,23 @@ public class Ejer9 {
 	//creamos una funcion para averiguar si es el idioma javalandia
 	static boolean esJavalandia(String frase) {
 		
+		//creamos los int fin e inicio
+		int inicio = 0;
+		int fin = 0;
+		
 		//creamos un string que quite las pausas del idioma
 		String respaldoFrase = frase.replaceAll(" ", "");
 		
 		//creamos un booleano que se devolvera al llamar a la funcion
-		boolean respuesta = true;
+		boolean respuesta = false;
 		
 		//buscamos la muletilla tipica del inicio y del fin de las frases
-		int inicio = respaldoFrase.indexOf("Javalín, javalón");
-		int fin = respaldoFrase.indexOf("javalén, len, len", 1);
+		inicio = respaldoFrase.indexOf("Javalín, javalón");
+		fin = respaldoFrase.indexOf("javalén, len, len", 1);
 
 		//comprobamos si de verdad es Javalandia
-		if (inicio == -1 && fin == -1) {
-			respuesta = false;
+		if (inicio != -1 && fin != -1) {
+			respuesta = true;
 		}
 		
 		//devolvemos el booleano
