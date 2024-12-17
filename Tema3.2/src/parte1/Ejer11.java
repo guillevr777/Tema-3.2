d5542a062d01ebde5f3307fd742e7117f092c088
guillevr777 <gs.villanueva@iesnervion.es>
2024-12-17 13:56:27 +0100

    upload

---
+++ Tema3.2/src/parte1/Ejer11.java
@@ -0,0 +1,11 @@
+package parte1;
+
+public class Ejer11 {
+
+	public static void main(String[] args) {
+
+	
+		
+	}
+
+}
--- Tema3.2/src/parte1/Ejer9.java
+++ Tema3.2/src/parte1/Ejer9.java
@@ -26,26 +26,15 @@ public class Ejer9 {
 	//creamos una funcion para averiguar si es el idioma javalandia
 	static boolean esJavalandia(String frase) {
 		
-		//creamos los int fin e inicio
-		int inicio = 0;
-		int fin = 0;
-		
-		//creamos un string que quite las pausas del idioma
-		String respaldoFrase = frase.replaceAll(" ", "");
-		
-		//creamos un booleano que se devolvera al llamar a la funcion
+		//creamos un boolean para devolver la respuesta
 		boolean respuesta = false;
-		
-		//buscamos la muletilla tipica del inicio y del fin de las frases
-		inicio = respaldoFrase.indexOf("Javalín, javalón");
-		fin = respaldoFrase.indexOf("javalén, len, len", 1);
-
-		//comprobamos si de verdad es Javalandia
-		if (inicio != -1 && fin != -1) {
-			respuesta = true;
-		}
-		
-		//devolvemos el booleano
-		return respuesta;
-	}
+		//creamos un string para guardar la frase sin espacios
+        String respaldoFrase = frase.replaceAll(" ", "").toLowerCase();
+        
+        //averiguamos el boolean es true o false
+        respuesta = respaldoFrase.startsWith("javalín,javalón") || respaldoFrase.endsWith("javalén,len,len");
+        
+        //devolvemos el boolean
+        return respuesta;
+    }
 }
