71a66a0c64ef3f538c62c3c6dd488bd639ee8f0f
guillevr777 <gs.villanueva@iesnervion.es>
2025-01-12 20:54:13 +0100

    Intentando remontar las tareas poco a poco

--- Tema3.2/src/parte1/Ejer12.java
+++ Tema3.2/src/parte1/Ejer12.java
@@ -12,17 +12,30 @@ public class Ejer12 {
 		//creamos la variable para almacenar un string que se recogera del usuario
 		String frase;
 		
+		//creamos un array de string
+		String palabras[];
+		
+		//string para alvergar la palabra mas larga
+		String palabraLarga = "";
+		
 		//pedimos el string
+		System.out.println("Dime una frase y te digo la palabra mas larga:");
 		frase = reader.nextLine();
 		
-		//llamamos a la funcion
-		imprimir(frase);
+		//usamos el metodo split para dividir las palabras
+		palabras = frase.split(" ");
 		
-	}
-	static void imprimir(String frase) {
-	
-		for (int i = 0 ; i < frase.length() ; i++) {
-			
+		//creamos un for para recorrer el array
+		for (int i = 0 ; i < palabras.length ; i++) {
+			if (palabraLarga.length() < palabras[i].length()) {
+				palabraLarga = palabras[i];
+			}
 		}
+		
+		// Mostramos la palabra más larga
+        System.out.println("La palabra más larga es: " + palabraLarga + " y tiene " + palabraLarga.length() + " letras.");
+		
+        //cerramos el escaner
+        reader.close();
 	}
 }
